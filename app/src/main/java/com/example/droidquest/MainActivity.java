81202312c55151f0e296e840046834a4736c9238
DanilKorn <90alegator7@gmail.com>
2024-05-08 09:49:43 +0300

    first commit

---
+++ app/src/main/java/com/example/droidquest/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.droidquest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_android, true),
+            new Question(R.string.question_linear, false),
+            new Question(R.string.question_service, false)
+            new Question(R.string.question_res, true),
+            new Question(R.string.question_manifest, true),
+    };
+    private int mCurrentIndex = 0;
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResID();
+        mQuestionTextView.setText(question);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mQuestionTextView =
+                (TextView)findViewById(R.id.question_text_view);
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+        updateQuestion();
+    }
+    }
